3989c6df44fdaffd6ea7275a5caf3f32224a9570
Deblina Sarkar <60690712+deblinasarkar@users.noreply.github.com>
2022-02-25 09:04:29 +0530

    Update ex4.java

--- LABBOOK/src/lab2/ex4.java
+++ LABBOOK/src/lab2/ex4.java
@@ -2,6 +2,34 @@ package lab2;
 import java.util.*;
 
 public class ex4 {
-int n;
-
+	public static void main(String[] args) {
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		ex4.modifyArray(a);
+	}
+	public static void modifyArray(int[] a)
+	{
+		    
+		    int[] b=new int[a.length];
+		    int c=0;
+        	Arrays.sort(a);
+        	for (int i=0;i<a.length-1;i++) {
+            	if (a[i] != a[i + 1]) {
+                	b[c] = a[i];
+                	c++;
+            }
+        }
+         b[c] = a[a.length - 1];
+        for (int i = 0; i <= c; i++) {
+            System.out.println(b[i]);
+        }  
 }
+}
+
+ 
